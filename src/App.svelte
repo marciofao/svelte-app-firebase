f0e011c3ecae1270222e990b72d9cff86e82b92b
marciofao <marcio.lopes.fao@gmail.com>
2023-03-27 15:06:09 -0300

    create logon/signup form

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,22 @@
 <script>
-	export let name;
+  export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <form class="signup">
+    <label for="email">Email</label>
+    <input type="email" name="email" />
+    <label for="password">Password:</label>
+    <input type="password" name="password" />
+    <button>Signup</button>
+  </form>
+  <h2>Login</h2>
+  <form class="login">
+    <label for="email">Email</label>
+    <input type="email" name="email" />
+    <label for="password" name="password">Password</label>
+    <input type="password" name="password" />
+    <button>Login</button>
+  </form>
+  <button class="logout">Logout</button>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
